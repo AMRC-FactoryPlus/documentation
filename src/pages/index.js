d74638dc55f27e9461b13a6802027b68e76bd996
Alex Godbehere <alex.godbehere@gmail.com>
2023-04-27 10:44:26 +0100

    Add Foreword

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,9 +8,18 @@ import Particles from "react-tsparticles";
 import {loadFull} from "tsparticles";
 
 import {library} from '@fortawesome/fontawesome-svg-core'
-import {faRocket} from '@fortawesome/free-solid-svg-icons'
+import {
+    faBinoculars,
+    faCircleDot, faCircleNodes, faCircleNotch,
+    faCode, faDatabase,
+    faFileCode,
+    faGear,
+    faLock,
+    faRocket, faServer, faWarehouse
+} from '@fortawesome/free-solid-svg-icons'
 import {faArrowRight} from '@fortawesome/free-solid-svg-icons'
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faGithub} from "@fortawesome/free-brands-svg-icons/faGithub";
 
 library.add(faRocket)
 library.add(faArrowRight)
@@ -124,13 +133,16 @@ export function Header() {
                         className="bg-brand dark:bg-brand-3 px-2.5 py-1 text-xs font-semibold text-white dark:text-brand tracking-wide uppercase">
                       What's new?
                     </span>
-                        <span className="inline-flex items-center text-sm font-medium text-brand dark:text-brand-3 space-x-1">
+                                <span
+                                    className="inline-flex items-center text-sm font-medium text-brand dark:text-brand-3 space-x-1">
                             <span>Re-engineered from the ground up</span>
                         </span>
                             </a>
                         </div>
                         <p className="mt-3 max-w-md text-base text-brand-80 dark:text-brand-3 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
-                            <span className={'font-black'}>Factory+</span> is a cutting-edge open-source reference framework that establishes the digital blueprints necessary for a state-of-the-art, connected manufacturing facility.
+                            <span className={'font-black'}>Factory+</span> is a cutting-edge open-source reference
+                            framework that establishes the digital blueprints necessary for a state-of-the-art,
+                            connected manufacturing facility.
                         </p>
                         <div className="flex flex-col sm:flex-row  sm:justify-start gap-4 mt-10">
                             <Link className={'hover:no-underline'}
@@ -155,95 +167,6 @@ export function Header() {
                                 </div>
                             </Link>
                         </div>
-                    </div>
-                </main>
-            </div>
-        </div>
-    );
-}
-
-export function Links() {
-    return (
-        <div className="relative overflow-hidden">
-            <div className="relative 2xl:pb-16 flex items-center justify-center">
-                <main
-                    className="mt-10 flex-1 max-w-7xl sm:mt-16 grid-cols-3 gap-16 flex flex-col lg:flex-row justify-between pt-4 mb-20 mx-0 px-0">
-                    <div className={'w-full col-span-3 2xl:col-span-1 mb-8 2xl:mb-0'}>
-                        <div className="flex items-center justify-start mb-2">
-                            <span className="text-xl text-gray-700 font-semibold mr-2">Core Framework</span>
-                            <span
-                                className="invisible sm:visible bg-brand px-2 py-1 text-xs uppercase tracking-wide text-white font-bold">
-                  UPDATED
-                </span>
-                        </div>
-                        <div className="text-gray-400 mb-6 2xl:h-24 overflow-ellipsis">The blueprint for Factory+. The
-                            Core Framework is a technical document outlining the high-level concepts.
-                        </div>
-                        <Link className={'hover:no-underline'}
-                              to={useBaseUrl('/core-framework/framework/about')}>
-                            <div className="  mb-3">
-                                <div
-                                    className="hover:no-underline w-full flex whitespace-nowrap items-center justify-center px-8 py-3 text-base font-medium  text-brand bg-white border border-brand hover:bg-brand hover:text-white md:py-4 md:text-lg md:px-10"
-                                >
-                                    Core Framework
-                                </div>
-                            </div>
-                        </Link>
-                        <Link className={'hover:no-underline'}
-                              to={useBaseUrl('/core-framework/implementation-guide/about')}>
-                            <div className=" ">
-                                <div
-                                    className="mb-4 sm:mb-0 hover:no-underline w-full flex whitespace-nowrap items-center justify-center px-8 py-3 text-base font-medium  text-brand bg-white border border-brand hover:bg-brand hover:text-white md:py-4 md:text-lg md:px-10"
-                                >
-                                    Implementation Guide
-                                </div>
-                            </div>
-                        </Link>
-                    </div>
-                    <div className={'w-full col-span-3 2xl:col-span-1 mb-8 2xl:mb-0'}>
-                        <div className="flex items-center justify-start mb-2">
-                            <span className="text-xl text-gray-700 font-semibold mr-2">Consumption Framework</span>
-                            <span
-                                className="invisible sm:visible bg-brand px-2 py-1 rounded-full text-xs uppercase tracking-wide text-white font-bold">
-                  NEW
-                </span>
-                        </div>
-                        <div className="text-gray-400 2xl:h-24 mb-6">A standardised set of services to enable
-                            applications
-                            to discover the data they are interested in and understand how to make use of it.
-                        </div>
-                        <Link className={'hover:no-underline'}
-                              to={useBaseUrl('/consumption-framework')}>
-                            <div className="  mb-3">
-                                <div
-                                    className="mb-4 sm:mb-0 hover:no-underline w-full flex whitespace-nowrap items-center justify-center px-8 py-3 text-base font-medium  text-brand bg-white border border-brand hover:bg-brand hover:text-white md:py-4 md:text-lg md:px-10"
-                                >
-                                    Consumption Framework
-                                </div>
-                            </div>
-                        </Link>
-                    </div>
-                    <div className={'w-full col-span-3 2xl:col-span-1 mb-8 2xl:mb-0'}>
-                        <div className="flex items-center justify-start mb-2">
-                            <span className="text-xl text-gray-700 font-semibold mr-2">Showcase</span>
-                            <span
-                                className="invisible sm:visible bg-brand px-2 py-1 rounded-full text-xs uppercase tracking-wide text-white font-bold">
-                  NEW
-                </span>
-                        </div>
-                        <div className="text-gray-400 2xl:h-24 mb-6">The Factory+ showcase is a collection of projects
-                            made possible by leveraging the Factory+ Framework.
-                        </div>
-                        <Link className={'hover:no-underline'}
-                              to={useBaseUrl('/showcase')}>
-                            <div className="  mb-3">
-                                <div
-                                    className="mb-4 sm:mb-0 hover:no-underline w-full flex whitespace-nowrap items-center justify-center px-8 py-3 text-base font-medium  text-brand bg-white border border-brand hover:bg-brand hover:text-white md:py-4 md:text-lg md:px-10"
-                                >
-                                    Showcase
-                                </div>
-                            </div>
-                        </Link>
                     </div>
                 </main>
             </div>
@@ -293,8 +216,7 @@ function Home() {
             title={`Home`}
             description="Factory+">
             <Header></Header>
-            <Links></Links>
-            {/*<Testimonial></Testimonial>*/}
+            <Testimonial></Testimonial>
         </Layout>
     );
 }
